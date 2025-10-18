55b68f3a00a29c8a2efed9a14f845f514471d4da
“Marcus” <115784464+Marcussy34@users.noreply.github.com>
2025-10-18 17:16:11 +0800

    updated browser tab to show DhalWay logo

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* DhalWay branding */}
+        <title>DhalWay</title>
+        <meta name="description" content="Multi-chain crypto payment preferences" />
+        <link rel="icon" type="image/png" href="/icons/dhalway_1.png" />
+        <link rel="apple-touch-icon" href="/icons/dhalway_1.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
