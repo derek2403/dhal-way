e9ca9b75b7c2895545be014b6ca064faf635c356
“Marcus” <115784464+Marcussy34@users.noreply.github.com>
2025-10-19 12:47:03 +0800

    added navbar to transfer.js

--- pages/transfer.js
+++ pages/transfer.js
@@ -269,7 +269,7 @@ export default function Transfer() {
         />
       </div>
       <div className="relative z-10">
-        <Header />
+        <Header showNavigation={true} />
         <main className="px-4 sm:px-6 lg:px-8 py-4 sm:py-6 lg:py-8 max-w-7xl mx-auto">
         <div className="space-y-6 sm:space-y-8 lg:space-y-10">
           {/* Portfolio Info from QR Code */}
